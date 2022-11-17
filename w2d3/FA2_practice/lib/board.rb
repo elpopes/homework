b23f3675f7f466211423ccb70aae3613538d0a48
lorenzo <lorenzotijerina@gmail.com>
2022-11-16 22:33:49 -0500

    run test first time, remember how to spell initialize

--- w2d3/FA2_practice/lib/board.rb
+++ w2d3/FA2_practice/lib/board.rb
@@ -1,9 +1,37 @@
 class Board
+    attr_reader :max_height
+    def self.build_stacks(num_stacks)
+        Array.new(num_stacks){Array.new}
+    end
 
+    def initialize(num_stacks, max_height)
+        raise 'rows and cols must be >=4' if num_stacks < 4 || max_height < 4
+        @max_height = max_height
+        @stacks = Board.build_stacks(num_stacks)
+    end
 
-    # This Board#print method is given for free and does not need to be modified
-    # It is used to make your game playable.
     def print
         @stacks.each { |stack| p stack }
+    end
+
+    def add(token, stack_ids)
+        if @stacks[stack_ids].length < @max_height
+            @stacks[stack_ids] << token
+            return true
+        else
+            return false
+        end
+    end
+
+    def vertical_winner?(token)
+        @stacks.any? {|stack| @max_height == stack.count {|ele| ele == token}}
+    end
+
+    def horizontal_winner?(token)
+        (0...@stacks.length).any? {|index| @stacks.all? {|stack| stack[index] == token}}
+    end
+
+    def winner?(token)
+        vertical_winner?(token) || horizontal_winner?(token)
     end
 end
--- w2d3/FA2_practice/lib/connect_tac_toe.rb
+++ w2d3/FA2_practice/lib/connect_tac_toe.rb
@@ -3,9 +3,24 @@ require_relative 'player'
 
 class ConnectTacToe
 
+    def initialize(num_stacks, max_height)
+        @board = Board.new(num_stacks, max_height)
+        @player_1 = Player.new('y')
+        @player_2 = Player.new('b')
+        @current_player = @player_1
+    end
 
-    # This ConnectTacToe#play method is given for free and does not need to be modified
-    # It is used to make your game playable.
+    def switch_players! 
+        @current_player == @player_1 ? @current_player = @player_2 : @current_player = @player_1
+    end
+
+    def play_turn
+        move = @current_player.get_stack_index
+        @board.add(@current_player.token, move)
+        switch_players!
+    end
+
+
     def play
         until @board.winner?(@player_1.token) || @board.winner?(@player_2.token)
             @board.print
